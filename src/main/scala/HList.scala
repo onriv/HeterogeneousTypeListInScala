66896857263bdab2ee05317dbf21d5f0994bf307
Shanghe Chen <chenshanghe@outlook.com>
2018-01-06 01:04:42 +0800

    init and last view, type part

--- src/main/scala/HList.scala
+++ src/main/scala/HList.scala
@@ -2,20 +2,56 @@
   * Created by river on 17/4/16.
   */
 
-sealed trait HList
-
-final case class HCons[H, T <: HList](head: H, tail: T) extends HList {
-  def ::[T](v: T) = HCons(v, this)
-  override def toString: String = s"${head} :: ${tail}"
-}
-
-final class HNil extends HList {
-  def ::[T](v: T) = HCons(v, this)
-  override def toString: String = "HNil"
-}
-
 object HList {
   type ::[H, T <: HList] = HCons[H, T]
   val :: = HCons
   val HNil = new  HNil
 }
+
+sealed trait HList {
+  type Expand[IfHCons <: Up, IfHNil <: Up, Up] <: Up
+  type AsInitAndLast <: InitAndLastView
+}
+
+final case class HCons[H, T <: HList](head: H, tail: T) extends HList {
+  type Expand[IfHCons <: Up, IfHNil <: Up, Up] = IfHCons
+  type AsInitAndLast = T#Expand[
+    InitAndLastViewHCons[H, T#AsInitAndLast],
+    InitAndLastViewHNil[H],
+    InitAndLastView
+    ]
+  def ::[T](v: T) = HCons(v, this)
+  override def toString: String = s"${head} :: ${tail}"
+}
+
+final class HNil extends HList {
+  type Expand[IfHCons <: Up, IfHNil <: Up, Up] = IfHNil
+  def ::[T](v: T) = HCons(v, this)
+  override def toString: String = "HNil"
+}
+
+trait InitAndLastView {
+  type init <: HList
+  type last
+  def last: last
+  def init: init
+}
+
+class InitAndLastViewHNil[H](val x: H) extends InitAndLastView {
+  import HList._
+  type init = HNil
+  type last = H
+  def last = x
+  def init = HNil
+}
+
+class InitAndLastViewHCons[H, NextLastView <: InitAndLastView](
+    val x: H, val v: NextLastView) extends InitAndLastView {
+  import HList._
+  type init = H :: NextLastView#init
+  type last = NextLastView#last
+  def last = v.last
+  def init = HCons(x, v.init) // 为何这里不能写 x :: v.init
+}
+
+
