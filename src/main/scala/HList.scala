aa1b76a5f5b4157b727e78a4be78205153949e40
Shanghe Chen <chenshanghe@outlook.com>
2018-01-08 23:00:38 +0800

    append view, finished

--- src/main/scala/HList.scala
+++ src/main/scala/HList.scala
@@ -13,11 +13,18 @@ object HList {
     new InitAndLastViewHCons[H, Prev](x.head, prev(x.tail))
   }
 
+  implicit def append0[H, T](x : H :: HNil, t: T) =
+    new AppendViewN(x.head, new AppendView0(t))
+  implicit def appendN[H, T <: HList, T1, Prev <: AppendView]
+      (x : H :: T, t: T1)(implicit prev: (T, T1) => Prev) =
+    new AppendViewN(x.head, prev(x.tail, t))
+
 }
 
 sealed trait HList {
   type Expand[IfHCons <: Up, IfHNil <: Up, Up] <: Up
   type AsInitAndLast <: InitAndLastView
+  type Append[T] <: AppendView
 }
 
 final case class HCons[H, T <: HList](head: H, tail: T) extends HList {
@@ -29,9 +36,16 @@ final case class HCons[H, T <: HList](head: H, tail: T) extends HList {
     InitAndLastViewHNil[H],
     InitAndLastView
     ]
+  type Append[T1] = AppendViewN[H,T#Append[T1]]
+//  type Append[T1] = FullType#Expand[
+//    AppendViewN[H,T#Append[T1]],
+//    AppendView0[T1],
+//    AppendView
+//    ]
   def asInitAndLast(implicit in: FullType => FullType#AsInitAndLast): FullType#AsInitAndLast = in(this)
   def init(implicit in: FullType => FullType#AsInitAndLast) = in(this).init
 //  def last(implicit in: FullType => FullType#AsInitAndLast): AsInitAndLast#last = in(this).last
+  def append[T](t: T)(implicit in: (FullType, T) => FullType#Append[T]) = in(this, t)
   def ::[T](v: T) = HCons(v, this)
   override def toString: String = s"${head} :: ${tail}"
 }
@@ -39,6 +53,8 @@ final case class HCons[H, T <: HList](head: H, tail: T) extends HList {
 final class HNil extends HList {
   type Expand[IfHCons <: Up, IfHNil <: Up, Up] = IfHNil
   def ::[T](v: T) = HCons(v, this)
+  type Append[T] = AppendView0[T]
+  def append[T](t: T) = new AppendView0[T](t)
   override def toString: String = "HNil"
 }
 
@@ -64,6 +80,23 @@ class InitAndLastViewHCons[H, NextLastView <: InitAndLastView](
   type last = NextLastView#last
   def last = v.last
   def init = HCons(x, v.init) // 为何这里不能写 x :: v.init
+}
+
+sealed trait AppendView {
+  type Appended <: HList
+  def get: Appended
+}
+
+class AppendView0[T](val t : T) extends AppendView {
+  import HList._
+  type Appended = T :: HNil
+  def get = t :: HNil
+}
+
+class AppendViewN[H, A <: AppendView](val h: H, val a: A) extends AppendView {
+  import HList._
+  type Appended = H :: A#Appended
+  def get = HCons(h, a.get)
 }
 
 object Main1 extends App {
